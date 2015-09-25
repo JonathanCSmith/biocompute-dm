b43d9c9453921f937809c477c84583241190a79b
JonathanCSmith <jon@jonathansmith.me>
2015-09-25 09:38:32 +0100

    Updated the mysql connector to use a configuration handler for connection properties. Placed a template in /template Target directory for this file is: ~/biocis/ConfigurationFile.txt

--- cgi-bin/API/runQuery.py
+++ cgi-bin/API/runQuery.py
@@ -1,19 +1,24 @@
 # file runQuery.py
 
-
+# Generic function for running a query
 def runQuery(DBquery):
-    # print DBquery
-
     import MySQLdb as msd
-    # print "<p>",DBquery,"</p>"
+
+    # Obtain the connection properties
+
+    # Obtain a connection to the database
     DBname = "coreInSys"
     con = msd.connect('localhost', 'msl_user', 'msl_pass', DBname);
     cur = con.cursor()
 
+    # Execute the query
     cur.execute(DBquery)
 
+    # Commit the query and return the result
     con.commit()
     res = cur.fetchall()
+
+    # Close the query
     cur.close()
 
     return (res)
