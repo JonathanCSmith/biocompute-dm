7b122cd66c4b34e0cd2a795e4d2c90abaeee18b1
JonathanCSmith <jon@jonathansmith.me>
2016-02-17 15:59:03 +0000

    testing relative directories in our test1:module1, should this be acceptable will move to file and i/o testing

--- pipeline_scripts/test_pipeline/test.sh
+++ pipeline_scripts/test_pipeline/test.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-echo 'ticket: "${ticket}"'
-echo 'working directory: "${working_directory}"'
-echo "samples: '${samples}'"
-echo cwd
-echo pwd
-echo "t: '${t}'"
-echo "b: '${b}'"
-echo "l: '${l}'"
-echo "f: '${f}'"
+echo "ticket: ${ticket}"
+echo "working directory: ${working_directory}"
+echo "samples: ${samples}"
+echo "actual working directory: $PWD"
+echo "t: ${t}"
+echo "b: ${b}"
+echo "l: ${l}"
+echo "f: ${f}"
 
