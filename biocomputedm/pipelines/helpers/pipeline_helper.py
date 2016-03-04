10929bcd4057d5d736e9c87df3e84b33253ced68
JonathanCSmith <jon@jonathansmith.me>
2016-03-04 16:23:58 +0000

    Was not forwarding the pipeline script locations

--- biocomputedm/pipelines/helpers/pipeline_helper.py
+++ biocomputedm/pipelines/helpers/pipeline_helper.py
@@ -281,6 +281,7 @@ def execute_pipeline_instance(app, pid="", oid=""):
             remote_modules_output_directory = os.path.join(remote_pipeline_directory, "modules_output")
             remote_module_directory = os.path.join(remote_modules_output_directory, current_module_instance.module.name)
 
+            pipeline_scripts = os.path.join(os.path.join(utils.get_path("scripts", "hpc"), "pipelines"), pipeline_instance.pipeline.name)
             with open(os.path.join(local_module_directory,
                                    current_module_instance.module.name + "_hpc_submission_out.log"), "wb") as out, \
                     open(os.path.join(local_module_directory,
@@ -294,6 +295,7 @@ def execute_pipeline_instance(app, pid="", oid=""):
                             "-t=" + current_module_instance.display_key,
                             "-e=" + current_module_instance.module.executor,
                             "-l=" + local_module_directory,
+                            "-p=" + pipeline_scripts,
                             "-w=" + remote_pipeline_directory,
                             "-o=" + remote_module_directory,
                             "-i=" + csv_path,
