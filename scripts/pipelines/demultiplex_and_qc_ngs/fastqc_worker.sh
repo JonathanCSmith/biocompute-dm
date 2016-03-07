b5276e4fa9be5de6119bf19e83b26c905eb3d571
JonathanCSmith <jon@jonathansmith.me>
2016-03-07 16:51:42 +0000

    worker needs some love as it doesn't seem to be picking up the files as we expect

--- scripts/pipelines/demultiplex_and_qc_ngs/fastqc_worker.sh
+++ scripts/pipelines/demultiplex_and_qc_ngs/fastqc_worker.sh
@@ -5,15 +5,19 @@
 
 # Debug
 echo "Array ID: ${SGE_TASK_ID}"
+echo "File List: ${FILE_LIST}"
 
 # Parse the file path
 OLFIFS="${IFS}"
 IFS=","
 read -a FILE_PATHS <<< "${FILE_LIST}"
 IFS="${OLFIFS}"
+echo "File Paths: ${FILE_PATHS}"
+
 FILE_PATH=${FILE_PATHS[${SGE_TASK_ID}]}
+echo "Working on file: ${FILE_PATH}"
 
 # Run FastQC on our specific file
 module load bioinformatics/FastQC/0.11.3
-fastqc ${FILE_PATH}
+fastqc "${FILE_PATH}"
 
