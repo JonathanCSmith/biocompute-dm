710c17805fdaebe486c8679c4b6655630fac8401
JonathanCSmith <jon@jonathansmith.me>
2016-07-21 11:00:38 +0100

    flatten bcl2fastq2

--- scripts/pipelines/demultiplex_and_qc_ngs/bcl2fastq2.sh
+++ scripts/pipelines/demultiplex_and_qc_ngs/bcl2fastq2.sh
@@ -129,4 +129,9 @@ ulimit -n 4096
 module load bioinformatics/bcl2fastq2/2.17.1.14
 bcl2fastq ${EXECUTION_VARIABLES} || post_error
 
+# Flatten bcl2fastq2s stupid output directory structure
+if [ find "${DATA_OUTPUT_DIRECTORY}" -type d ]; then
+    find "${DATA_OUTPUT_DIRECTORY}" -mindepth 2 -type f -exec mv -i '{}' "${DATA_OUTPUT_DIRECTORY}" ';'
+fi
+
 IFS="${OLDIFS}"
